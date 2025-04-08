983bbf4336cc0006e2c8beb1ab0714e91fc4e979
Ritvik0771 <64149255+Ritvik0771@users.noreply.github.com>
2025-04-08 13:36:47 +0000

    Update JreUpdateChecker.java

--- JreUpdateChecker.java
+++ JreUpdateChecker.java
@@ -3,6 +3,7 @@ package com.ncratleos.baseimage.jre;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ncratleos.baseimage.dto.AlpineImageInfoDto;
 import com.ncratleos.baseimage.service.GithubService;
 import io.micrometer.common.util.StringUtils;
 import com.ncratleos.baseimage.service.JreService;
@@ -19,7 +20,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
 import java.nio.file.*;
+import java.security.PublicKey;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -59,7 +63,7 @@ public class JreUpdateChecker {
     public void checkJreUpdate(String githubAuthToken) throws JreServiceException {
         try{
             String targetDirectory = System.getProperty("java.io.tmpdir") + "/jre/";
-//            jreService.downloadAndExtractJDK(targetDirectory);
+            jreService.downloadAndExtractJDK(targetDirectory);
 
             // 2. Get Java version
             Path jdkHome = findJdkHome(Paths.get(targetDirectory));
@@ -69,7 +73,7 @@ public class JreUpdateChecker {
             log.info("Current Java version: {}", currentVersion);
 
             // 3. Get last JRE image details from GitHub
-            String lastVersion = githubService.getLastJreImageDigest(githubAuthToken);
+            String lastVersion = githubService.getLastJreImageVersion(githubAuthToken);
             log.info("Last Java version: {}", lastVersion);
 
 
@@ -82,12 +86,8 @@ public class JreUpdateChecker {
                  return;
              }
              // 4. Create and push new JRE image details JSON file
-             createNewJreImageJsonFile(currentVersion);
+             createNewJreImageJsonFile(currentVersion, githubAuthToken);
              githubService.pushNewJreImageFileToRepo(githubAuthToken);
-
-
-
-
 
         }catch (Exception e) {
              throw new JreServiceException("Failed to check JRE update", e);
@@ -135,15 +135,78 @@ public class JreUpdateChecker {
     }
 
 
-    private void createNewJreImageJsonFile(String version) {
+    public void createNewJreImageJsonFile(String version, String githubAuthToken) {
         Map<String, Object> data = new HashMap<>();
-        data.put("version", version);
+        data.put("jre-version", version);
+        data.put("alpine-image-repo", "fsg-ess-docker-releases");
+        data.put("alpine-image-path", "ncratleos.jfrog.io/fsg-ess-docker-releases/com/ncratleos/baseimages/alpine");
 
-        try (FileWriter writer = new FileWriter(NEW_JRE_JSON_FILENAME)) {
+        List<String> latestAlpineTags = githubService.getLastImageTags(githubAuthToken);
+        String alpineTags = String.join(",", latestAlpineTags);
+        data.put("alpine-tags",alpineTags);
+        data.put("jre-repo-path", "fsg-ess-docker-releases/com/ncratleos/baseimages/corretto/jre");
+
+        String jreTags = customJreTags(githubAuthToken, version, latestAlpineTags);
+
+        data.put("jre-image-tags", jreTags);
+
+        try (FileWriter writer = new FileWriter(System.getenv("GITHUB_ENV"), true)) {
             objectMapper.writeValue(new File(NEW_JRE_JSON_FILENAME), data);
             log.info("Created new JRE image details file: {}", NEW_JRE_JSON_FILENAME);
+            for (Map.Entry<String, Object> entry : data.entrySet()){
+                writer.write(entry.getKey() + "=" + entry.getValue().toString() + "\n");
+            }
         } catch (IOException e) {
             log.error("Failed to create new JRE image details file", e);
         }
+    }
+
+    public String customJreTags(String githubAuthToken, String version, List<String> latestAlpineTags){
+
+        List<String> jreTags = new ArrayList<>();
+        // fix values
+        String latestAlpine = "latest";
+        String fixJreVersion = "17";
+        String platformArchitecture = getPlatformArchitecture(githubAuthToken);
+        String os = "alpine";
+        String variableJreVersion = extractVersion(version);
+        String alpinetag = extractAlpineTag(latestAlpineTags);
+
+        jreTags.add(latestAlpine);
+        jreTags.add(fixJreVersion);
+        jreTags.add(fixJreVersion + "-" + os + "-" + alpinetag);
+        jreTags.add(fixJreVersion + "-" + os + "-" + alpinetag + "-" + platformArchitecture);
+        jreTags.add(variableJreVersion);
+        jreTags.add(variableJreVersion + "-" + os + "-" + alpinetag);
+        jreTags.add(variableJreVersion + "-" + os + "-" + alpinetag + "-" + platformArchitecture);
+
+        return String.join(",", jreTags);
+    }
+
+    public String getPlatformArchitecture(String githubAuthToken){
+        AlpineImageInfoDto alpineImageInfoDto = this.githubService.getAlpineManifest(githubAuthToken);
+        return alpineImageInfoDto.getPlatform().getArchitecture();
+
+    }
+
+    public String extractVersion(String version){
+        Pattern pattern = Pattern.compile("\\b(\\d+\\.\\d+\\.\\d+)\\b");
+        Matcher matcher = pattern.matcher(version);
+        if(matcher.find())
+            return matcher.group(1);
+        return null;
+    }
+
+    public String extractAlpineTag(List<String> latestAlpineTags){
+        String alpinetag = null ;
+        for(String tag : latestAlpineTags){
+            if(!tag.equals("latest")){
+                Pattern pattern = Pattern.compile("^(\\d+\\.\\d+)");
+                Matcher matcher = pattern.matcher(tag);
+                if(matcher.find())
+                    alpinetag = matcher.group(1);
+            }
+        }
+        return alpinetag;
     }
 }
